e696a327d0cd7e19e9ef782ce76d348d983fe09e
ogh826 <eys555@naver.com>
2025-03-25 18:32:10 +0900

    [Fix] 아처타워의 공격 대상 최근거리 적으로 수정

---
+++ Assets/Scripts/Tower/ArcherTower.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ArcherTower : Tower
+{
+    [SerializeField] Collider[] enemys;
+    private void Start()
+    {
+        name = "궁수타워";
+        attackDelay = 0.5f;
+        poolType = E_PoolType.Arrow;
+        range = 5f;
+        Damage = 1f;
+    }
+    private void Update()
+    {
+        enemys = Physics.OverlapSphere(transform.position, range, 1 << 3);
+        if (enemys.Length > 0)
+        {
+            enemys = enemys.OrderBy(enemy => (enemy.transform.position - transform.position).sqrMagnitude).ToArray();
+            // 거리가 가까운 것을 배열 첫번째 값으로 변경하면서 화살의 타겟으로 설정
+            enemy = enemys[0].gameObject;
+            Attack(this);
+        }
+    }
+
+
+    public override void Attack(Tower child)
+    {
+        if (isAttack == false)
+        {
+            GameObject obj = ObjectPoolManager.Instance.GetObject(poolType, transform);
+            if(obj.GetComponent<Bullet>().Tower == null)
+            {
+                obj.GetComponent<Bullet>().Tower = child;
+            }
+        }
+        base.Attack(child);
+    }
+}
