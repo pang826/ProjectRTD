c9cbf3aa71a246bb640471893db8679d45f3936d
ogh826 <eys555@naver.com>
2025-04-08 23:41:59 +0900

    [Feat] 캐논볼 스플레시 데미지 구현

--- Assets/Scripts/Bullet/CanonBall.cs
+++ Assets/Scripts/Bullet/CanonBall.cs
@@ -29,4 +29,32 @@ public class CanonBall : Bullet
         
         ObjectPoolManager.Instance.ReturnObject(this.poolType, gameObject);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            // 폭발 범위 판정
+            float explosionRadius = 2.0f;
+            Collider[] hits = Physics.OverlapSphere(transform.position, explosionRadius,1 << 3);
+
+            foreach (var hit in hits)
+            {
+                IDamageable target = hit.gameObject.GetComponent<IDamageable>();
+                if (target != null)
+                {
+                    Debug.Log("데미지");
+                    target.TakeDamage(damage);
+                }
+                else
+                {
+                    Debug.Log("널");
+                }
+            }
+
+            // 폭발 이펙트 등 추가 가능
+            // Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+            ObjectPoolManager.Instance.ReturnObject(this.poolType, gameObject);
+        }
+    }
 }
