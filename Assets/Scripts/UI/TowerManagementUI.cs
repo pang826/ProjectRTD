b6fba6add244a41bfea6595f5ca87c7e691bb726
pang <eys555@naver.com>
2025-04-17 16:30:19 +0900

    [Feat] 타워 매니지먼트 UI 숨기기/꺼내기 기능 구현

--- Assets/Scripts/UI/TowerManagementUI.cs
+++ Assets/Scripts/UI/TowerManagementUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,19 @@ public class TowerManagementUI : MonoBehaviour
     public TowerBase TowerBase;
     public Button BuyTowerButton;
     public Button UpgrageTowerButton;
+    public Button HideButton;
+    private TextMeshProUGUI hideButtonText;
+
+    private bool isHide;
+    private Vector3 unhidePos;
+    private Vector3 hidePos;
+
+    private void Awake()
+    {
+        unhidePos = transform.position;
+        hidePos = transform.position + new Vector3(458, 0, 0);
+        hideButtonText = HideButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+    }
 
     public void BuyTower()
     {
@@ -25,6 +39,48 @@ public class TowerManagementUI : MonoBehaviour
         if(TowerBase.isSpawned == true)
         {
 
+        }
+    }
+
+    public void HideUI()
+    {
+        if(isHide == false) 
+        {
+            StartCoroutine(HideRoutine());
+        }
+        else
+        {
+            StartCoroutine(UnhideRoutine());
+        }
+    }
+
+    IEnumerator HideRoutine()
+    {
+        while(true) 
+        {
+            transform.position = Vector3.Lerp(transform.position, hidePos, Time.deltaTime * 7f);
+            if(Vector3.Distance(transform.position, hidePos) <= 0.5f)
+            {
+                isHide = true;
+                hideButtonText.text = "¢¸";
+                yield break;
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator UnhideRoutine()
+    {
+        while(true)
+        {
+            transform.position = Vector3.Lerp(transform.position, unhidePos, Time.deltaTime * 7f);
+            if(Vector3.Distance(transform.position, unhidePos) <= 0.5f)
+            {
+                isHide = false;
+                hideButtonText.text = "¢º";
+                yield break;
+            }
+            yield return null;
         }
     }
 }
